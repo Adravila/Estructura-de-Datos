f0013fc33291cae0c5b5d75a6115b99c07479dba
Adrián <Adravila@users.noreply.github.com>
2019-01-28 15:36:57 +0100

    Update Tren

--- Lineales/TAD Completos/TAD Tren/Tren.h
+++ Lineales/TAD Completos/TAD Tren/Tren.h
@@ -2,70 +2,122 @@
 #define TREN_H_INCLUDED
 #include "Pila_enla.h"
 
-struct vagon{
-    int n_vagon;
+typedef int tVagon;
+
+template<typename tVagon>
+class Tren
+{
+    public:
+        Tren();
+        void DesplazarIzq();
+        void DesplazarDrch();
+        void InsertarVagon(const tVagon& v);
+        void EliminarVagon();
+        bool TrenVacio() const;
+        const tVagon& VagonActivo() const noexcept;
+        void MostrarTren(); // Exclusivo
+    private:
+        Pila<tVagon> P_IZQ, P_DRCH;
+        tVagon vagonActivo;
 };
 
-vagon vagonVacio{};
-// Toda estructura vacía generará sus atributos por defecto,
-// en este caso como n_vagon es entero por defecto tendrá 0.
+template<typename tVagon>
+Tren<tVagon>::Tren() : vagonActivo(-1) {}
 
-class Tren{
-public:
-    Tren();
-    void insertarVagon(const vagon& v);
-    void desplazarIzq();
-    void desplazarDer();
-    void eliminarVagon();
-    bool trenVacio() const;
-    const vagon& vagonActivo() const noexcept;
-private:
-    Pila<vagon>pDer;
-    Pila<vagon>pIzq;
-};
-
-inline Tren::Tren(){
-    Pila<vagon>pDer = Pila<vagon>();
-    Pila<vagon>pIzq = Pila<vagon>();
-}
-
-inline void Tren::insertarVagon(const vagon& v){ pDer.push(v); }
-
-void Tren::desplazarIzq(){
-    if(!pDer.vacia()){
-        vagon v_a = pDer.tope();
-        pDer.pop();
-        if(pDer.vacia())
-            pDer.push(v_a); // No se puede desplazar a la izquierda porque es el último vagón activo a la derecha.
-        else
-            pIzq.push(v_a); // Como hay más vagones a la derecha, el siguiente al actual será el vagón activo.
+template<typename tVagon>
+void Tren<tVagon>::DesplazarIzq()
+{
+    if(!P_DRCH.vacia())
+    {
+        P_IZQ.push(vagonActivo);
+        vagonActivo = P_DRCH.tope();
+        P_DRCH.pop();
     }
 }
 
-inline void Tren::desplazarDer(){
-    if(!pIzq.vacia()){
-        pDer.push(pIzq.tope());
-        pIzq.pop();
+template<typename tVagon>
+void Tren<tVagon>::DesplazarDrch()
+{
+    if(!P_IZQ.vacia())
+    {
+        P_DRCH.push(vagonActivo);
+        vagonActivo = P_IZQ.tope();
+        P_IZQ.pop();
     }
 }
 
-inline void Tren::eliminarVagon(){
-    if(!pDer.vacia())
-        pDer.pop();
-    else if(!pIzq.vacia()){
-        pDer.push(pIzq.tope());
-        pIzq.pop();
+template<typename tVagon>
+void Tren<tVagon>::InsertarVagon(const tVagon& v)
+{
+    if(vagonActivo != -1)
+    {
+        P_DRCH.push(vagonActivo);
     }
+    vagonActivo = v;    
 }
 
-inline bool Tren::trenVacio() const{
-    return pDer.vacia() && pIzq.vacia();
+template<typename tVagon>
+void Tren<tVagon>::EliminarVagon()
+{
+    if(!P_DRCH.vacia())
+    {
+        vagonActivo = P_DRCH.tope();
+        P_DRCH.pop();
+    }
+    else if(!P_IZQ.vacia())
+    {
+        vagonActivo = P_IZQ.tope();
+        P_IZQ.pop();  
+    }
+    else
+    {
+        vagonActivo = -1;
+    }
+    
 }
 
-inline const vagon& Tren::vagonActivo() const noexcept{
-    	if(!pDer.vacia())
-            return pDer.tope();
-        return vagonVacio;
+template<typename tVagon>
+bool Tren<tVagon>::TrenVacio() const
+{
+    return vagonActivo == -1;
+}
+
+template<typename tVagon>
+const tVagon& Tren<tVagon>::VagonActivo() const noexcept
+{
+    return vagonActivo;
+}
+
+template<typename tVagon>
+void Tren<tVagon>::MostrarTren()
+{
+	Pila<tVagon> i, d, aux;
+	
+	i = P_IZQ;
+	d = P_DRCH;
+	
+	while(!i.vacia())
+	{
+		aux.push(i.tope());
+		i.pop();
+	}
+	
+	std::cout << "Izq: ";
+	while(!aux.vacia()) 
+	{
+		std::cout << aux.tope() << " ";
+		aux.pop();
+	}
+	
+	std::cout << "\nActivo: " << vagonActivo << std::endl;
+	
+	std::cout << "Der: ";
+	while(!d.vacia()) 
+	{
+		std::cout << d.tope() << " ";
+		d.pop();
+	}
+    std::cout << std::endl;
 }
 
 #endif // TREN_H_INCLUDED
---
+++ Lineales/TAD Completos/TAD Tren/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "Pila_enla.h"
+#include "Tren.h"
+
+int main()
+{
+    Tren<tVagon> T;
+    T.InsertarVagon(1);
+    T.InsertarVagon(2);
+    T.InsertarVagon(3);
+    T.InsertarVagon(4);
+    T.InsertarVagon(5);
+    T.DesplazarIzq();
+    T.DesplazarIzq();
+    T.DesplazarIzq();
+    T.DesplazarDrch();
+    T.EliminarVagon();
+    T.EliminarVagon();
+    T.EliminarVagon();
+    //T.EliminarVagon();
+    //T.EliminarVagon();
+
+    T.MostrarTren();
+}
