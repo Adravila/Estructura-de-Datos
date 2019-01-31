b3c0b37ce8718adc8f6b777e35105f0a4c30cc87
Adrián <Adravila@users.noreply.github.com>
2019-01-31 13:50:03 +0100

    Consultorio

--- Lineales/TAD Completos/TAD Consultorio/consultorio.hpp
+++ Lineales/TAD Completos/TAD Consultorio/consultorio.hpp
@@ -1,64 +1,66 @@
 #include <iostream>
-#include "lista_enla.hpp"
-#include "cola_enla.hpp"
+#include "Lista_enla.h"
+#include "Cola_enla.h"
 #include <cassert>
 
-typedef int Paciente;
+typedef int tPaciente;
 
-struct Medico{
-	Cola<Paciente>cola;
+struct tMedico
+{
+	Cola<tPaciente>cola;
 	const char* nombre;
 };
 
-class Consultorio{
-public:
-	Consultorio(){}
-	void AltaMedico(const Medico& m);
-	void BajaMedico(const Medico& m);
-	void ListaEspera(const Paciente& pa, const Medico& m);
-	Paciente ConsultarPaciente(const Medico& m);
-	void AtenderPaciente(const Medico& m);
-	bool tienePacientes(const Medico& m);
-private:
-	Lista<Medico> med;
-	Lista<Medico>::posicion p;
+class Consultorio
+{
+	public:
+		Consultorio(){}
+		void AltaMedico(const tMedico& m);
+		void BajaMedico(const tMedico& m);
+		void ListaEspera(const tPaciente& pa, const tMedico& m);
+		tPaciente ConsultarPaciente(const tMedico& m);
+		void AtenderPaciente(const tMedico& m);
+		bool tienePacientes(const tMedico& m);
+	private:
+		Lista<tMedico> med;
+		Lista<tMedico>::posicion p;
 };
 
-bool operator == (const Medico& m1, const Medico& m2){
+bool operator == (const tMedico& m1, const tMedico& m2){
 	return m1.nombre == m2.nombre;
 }
 
-void Consultorio::AltaMedico(const Medico& m){
+void Consultorio::AltaMedico(const tMedico& m){
 	p = med.fin();
 	med.insertar(m,p);
 }
 
-void Consultorio::BajaMedico(const Medico& m){
+void Consultorio::BajaMedico(const tMedico& m){
 	p = med.buscar(m);
 	if(p != med.fin())
 		med.eliminar(p);
 }
 
-void Consultorio::ListaEspera(const Paciente& pa, const Medico& m){
+void Consultorio::ListaEspera(const tPaciente& pa, const tMedico& m){
 	p = med.buscar(m);
 	if(p != med.fin())
 		med.elemento(p).cola.push(pa);
 }
 
-Paciente Consultorio::ConsultarPaciente(const Medico& m){
+tPaciente Consultorio::ConsultarPaciente(const tMedico& m){
 	p = med.buscar(m);
 	assert(p != med.fin());
 	assert(!med.elemento(p).cola.vacia());
 	return med.elemento(p).cola.frente();
 }
 
-bool Consultorio::tienePacientes(const Medico& m){
+bool Consultorio::tienePacientes(const tMedico& m){
 	p = med.buscar(m);
 	assert(p != med.fin());
-	return med.elemento(p).cola.vacia();	
+	return !med.elemento(p).cola.vacia();	
 }
 
-void Consultorio::AtenderPaciente(const Medico& m){
+void Consultorio::AtenderPaciente(const tMedico& m){
 	p = med.buscar(m);
 	if(p != med.fin()){
 		assert(!med.elemento(p).cola.vacia());
---
+++ Lineales/TAD Completos/TAD Consultorio/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include "consultorio.hpp"
+
+int main()
+{
+    tMedico t1, t2, t3, t4;
+    t1.nombre = "Pablo";
+    t2.nombre = "Joaquin";
+    t3.nombre = "Elena";
+    t4.nombre = "Rosa";
+
+    tPaciente p1 = 1, p2 = 2, p3 = 3, p4 = 4, p5 = 5, p6 = 6, p7 = 7;
+
+    Consultorio c;
+    c.AltaMedico(t1);
+    c.AltaMedico(t2);
+    c.AltaMedico(t3);
+    c.AltaMedico(t4);
+    c.ListaEspera(p1,t1);
+    c.ListaEspera(p2,t1);
+    c.ListaEspera(p3,t1);
+    c.ListaEspera(p4,t3);
+    c.ListaEspera(p5,t4);
+    std::cout << "Cod paciente: " << c.ConsultarPaciente(t1) << std::endl;
+    std::cout << "Cod paciente: " << c.ConsultarPaciente(t3) << std::endl;
+    std::cout << "Cod paciente: " << c.ConsultarPaciente(t4) << std::endl;
+    c.BajaMedico(t1);
+    c.AltaMedico(t1);
+    c.ListaEspera(p3,t1);
+    c.AtenderPaciente(t1);
+    std::cout << "Tiene pacientes: ";
+    if(c.tienePacientes(t1))
+    {
+        std::cout << "Sí" << std::endl;
+        c.ConsultarPaciente(t1);
+    }
+    else
+    {
+      std::cout << "No" << std::endl;  
+    }
+    
+}
