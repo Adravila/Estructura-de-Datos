843fabfd9c0ea46f2cc1262a3c8e11a4b9633138
Adrián <Adravila@users.noreply.github.com>
2019-02-02 17:17:18 +0100

    Cubilete

--- Lineales/TAD Completos/TAD Cubilete/Cubilete.cpp
+++ Lineales/TAD Completos/TAD Cubilete/Cubilete.cpp
@@ -1,121 +1,71 @@
 #include <iostream>
-#include "pila_enla.h"
+#include "Pila_enla.h"
+
+using namespace std;
 
 struct tCubo
 {
-	bool boca_abajo;
+	int altura;
 	int anchura;
-	tCubo(int n, bool ba) : boca_abajo(ba), anchura(n) {}
+	tCubo(int n, int a) : altura(a), anchura(n) {}
 };
 
-bool cubileteValido(const Pila<tCubo> P)
+Pila<tCubo> Ordena(const Pila<tCubo>& P)
 {
-	Pila<tCubo> Paux;
-	Paux = P;
-	while(!Paux.vacia())
+	Pila<tCubo> Porg, Pnew, Paux;
+	Porg = P;
+
+	while(!Porg.vacia())
 	{
-		tCubo c = Paux.tope();
-		Paux.pop();
-		while(!Paux.vacia())
+		if(Porg.tope().altura < 0)
 		{
-			if(c.anchura == Paux.tope().anchura)
+			tCubo c = Porg.tope();
+			c.altura *= -1; 
+			Porg.pop();
+			Porg.push(c);
+		}
+		if(Pnew.vacia())
+		{
+			Pnew.push(Porg.tope());
+			Porg.pop();
+		}
+		else if(Pnew.tope().anchura < Porg.tope().anchura)
+		{
+			Paux.push(Pnew.tope());
+			Pnew.pop();
+		}
+		else
+		{
+			Pnew.push(Porg.tope());
+			Porg.pop();
+			while(!Paux.vacia())
 			{
-				return false;
-			}
-			else
-			{
+				Pnew.push(Paux.tope());
 				Paux.pop();
 			}
 		}
 	}
-	return true;
-}
-
-Pila<tCubo> apilarCubilete(const Pila<tCubo> P)
-{
-	Pila<tCubo>Porg, Pnew, Paux;
-	Porg = P;
-
-	if(cubileteValido(P))
-	{
-		while(!Porg.vacia())
-		{
-			tCubo c = Porg.tope();
-			if(c.boca_abajo == false)
-			{
-				c.boca_abajo = true;
-			}
-			
-			Porg.pop();
-
-			if(Pnew.vacia()) // Insertando el primer elemento en Pnew
-	 		{
-				Pnew.push(c);
-			}	
-	 		else
-			{
-	 			if(c.anchura < Pnew.tope().anchura) // Si la condición cumple es que el cubilete se posicionará justamente encima.
-	 			{
-					Pnew.push(c);
-				}	
-	 			else
-				{
-					while(c.anchura > Pnew.tope().anchura)
-					{
-						Paux.push(Pnew.tope());
-						Pnew.pop();
-						if(!Pnew.vacia())
-						{
-							if(Pnew.tope().anchura > c.anchura)
-							{
-								Pnew.push(c);
-							}
-						}
-						else
-						{
-							Pnew.push(c);
-						}
-					}
-					while(!Paux.vacia())
-					{
-						Pnew.push(Paux.tope());
-						Paux.pop();
-					}
-				}
-			}
-		}
-		return Pnew;
-	}
-	return P;
+	return Pnew;
 }
 
 int main()
 {
-	tCubo c1(5,false);
-	tCubo c2(2,false);
-	tCubo c3(9,false);
-	tCubo c4(8,false);
-	tCubo c5(1,false);
-	tCubo c6(7,false);
-	tCubo c7(4,false);
-	tCubo c8(3,false);
-	tCubo c9(6,false);
-
-	Pila<tCubo> PC;
-	PC.push(c1);
-	PC.push(c2);
-	PC.push(c3);
-	PC.push(c4);
-	PC.push(c5);
-	PC.push(c6);
-	PC.push(c7);
-	PC.push(c8);
-	PC.push(c9);
-
-	PC = apilarCubilete(PC);
-	while(!PC.vacia())
+	Pila<tCubo> P;
+	tCubo t1(4,4), t2(9,-5), t3(3,-6), t4(1,4), t5(10,5), t6(20,5), t7(2,-3), t8(7,2);
+	P.push(t1);
+	P.push(t2);
+	P.push(t3);
+	P.push(t4);
+	P.push(t5);
+	P.push(t6);
+	P.push(t7);
+	P.push(t8);
+	
+	P = Ordena(P);
+	
+	while(!P.vacia())
 	{
-		std::cout << PC.tope().anchura;
-		PC.pop();
-	}
+		cout << "Anchura: " << P.tope().anchura << " y altura: " << P.tope().altura << endl;
+		P.pop();
+	}	
 }
