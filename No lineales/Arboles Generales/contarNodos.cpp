fa0de156a16ae4d6be4674637b17a80874d775d8
Adrián <Adravila@users.noreply.github.com>
2019-04-25 00:25:05 +0200

    Update contarNodos.cpp

--- No lineales/Arboles Generales/contarNodos.cpp
+++ No lineales/Arboles Generales/contarNodos.cpp
@@ -5,7 +5,6 @@
 
 using namespace std;
 
-
 /**
  * Contar los nodos que hay en todo el árbol general
  * Creado por: Adrián Dávila Guerra ©
@@ -18,18 +17,18 @@ int contarNodosRec(const Agen<T> &A, typename Agen<T>::nodo n)
 	int cont = 0;
 	typename Agen<T>::nodo hijo;
 	if (n == Agen<T>::NODO_NULO)
+	{
 		return 0;
+	}
 	else
 	{
 		hijo = A.hijoIzqdo(n);
-		++cont;
-
 		while (hijo != Agen<T>::NODO_NULO)
 		{
 			cont += contarNodosRec(A, hijo);
 			hijo = A.hermDrcho(hijo);
 		}
-		return cont;
+		return 1 + cont;
 	}
 }
 
@@ -53,10 +52,10 @@ int contarNodos(const Agen<T> &A)
 
 int main()
 {
-	Agen<char> A,B,C,D;
+	Agen<char> A, B, C, D;
 	cout << "\n*** Lectura de árbol general A de agen.dat ***\n";
-	ifstream fa("agen.dat"); // abrir fichero de entrada
-	rellenarAgen(fa, A);	 // desde fichero
+	ifstream fa("Data/agen.dat"); // abrir fichero de entrada
+	rellenarAgen(fa, A);		  // desde fichero
 	fa.close();
 	cout << "\n*** Mostrar árbol general A ***\n";
 	imprimirAgen(A); // en std::cout
@@ -64,7 +63,7 @@ int main()
 
 	cout << "\n*** Lectura de árbol general B de agen-ter.dat ***\n";
 	ifstream fb("Data/agen-ter.dat"); // abrir fichero de entrada
-	rellenarAgen(fb, B);	 // desde fichero
+	rellenarAgen(fb, B);			  // desde fichero
 	fb.close();
 	cout << "\n*** Mostrar árbol general B ***\n";
 	imprimirAgen(B); // en std::cout
@@ -72,7 +71,7 @@ int main()
 
 	cout << "\n*** Lectura de árbol general C de agen-cua.dat ***\n";
 	ifstream fc("Data/agen-cua.dat"); // abrir fichero de entrada
-	rellenarAgen(fc, C);	 // desde fichero
+	rellenarAgen(fc, C);			  // desde fichero
 	fc.close();
 	cout << "\n*** Mostrar árbol general C ***\n";
 	imprimirAgen(C); // en std::cout
@@ -80,7 +79,7 @@ int main()
 
 	cout << "\n*** Lectura de árbol general D de agen-abu.dat ***\n";
 	ifstream fd("Data/agen-abu.dat"); // abrir fichero de entrada
-	rellenarAgen(fd, D);	 // desde fichero
+	rellenarAgen(fd, D);			  // desde fichero
 	fd.close();
 	cout << "\n*** Mostrar árbol general D ***\n";
 	imprimirAgen(D); // en std::cout
