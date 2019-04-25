5a9c39539ceb32953ca5505d9b6ab74d6dd81eb8
Adrián <Adravila@users.noreply.github.com>
2019-04-25 16:57:30 +0200

    Update

--- No lineales/Arboles Parcialmente Ordenados/esAPO.cpp
+++ No lineales/Arboles Parcialmente Ordenados/esAPO.cpp
@@ -18,35 +18,84 @@ bool sonDescendientes(typename Abin<T>::nodo n, const Abin<T> &A)
         if (A.hijoIzqdoB(n) != Abin<T>::NODO_NULO)
         {
             if (A.elemento(n) >= A.elemento(A.hijoIzqdoB(n)))
+            {
                 desciende = false;
+            }
         }
         if (A.hijoDrchoB(n) != Abin<T>::NODO_NULO)
         {
             if (A.elemento(n) >= A.elemento(A.hijoDrchoB(n)))
+            {
                 desciende = false;
+            }
         }
         return desciende && sonDescendientes(A.hijoIzqdoB(n), A) && sonDescendientes(A.hijoDrchoB(n), A);
     }
 }
+
 template <typename T>
-int maxDesequilibrio(typename Abin<T>::nodo n, const Abin<T> &A)
+bool RamaIzqd(typename Abin<T>::nodo n, const Abin<T> &A)
 {
     if (n == Abin<T>::NODO_NULO)
-        return 0;
+        return true;
     else
     {
-        return max(max(abs(A.alturaB(A.hijoIzqdoB(n)) - A.alturaB(A.hijoDrchoB(n))), maxDesequilibrio(A.hijoIzqdoB(n), A)), maxDesequilibrio(A.hijoDrchoB(n), A));
+        bool completo = true;
+        if (A.hijoIzqdoB(n) != Abin<T>::NODO_NULO)
+        {
+            if (A.hijoDrchoB(n) == Abin<T>::NODO_NULO)
+            {
+                completo = false;
+            }
+        }
+        else if ((A.hijoIzqdoB(n) != Abin<T>::NODO_NULO && A.hijoDrchoB(n) == Abin<T>::NODO_NULO) || (A.hijoIzqdoB(n) == Abin<T>::NODO_NULO && A.hijoDrchoB(n) != Abin<T>::NODO_NULO))
+        {
+            completo = false;
+        }
+        return completo && RamaIzqd(A.hijoIzqdoB(n), A) && RamaIzqd(A.hijoDrchoB(n), A);
     }
+}
+
+template <typename T>
+bool RamaDrch(typename Abin<T>::nodo n, const Abin<T> &A)
+{
+    if (n == Abin<T>::NODO_NULO)
+    {
+        return true;
+    }
+    else
+    {
+        bool completo = true;
+        if (A.hijoDrchoB(n) != Abin<T>::NODO_NULO)
+        {
+            if (A.hijoIzqdoB(n) == Abin<T>::NODO_NULO)
+            {
+                completo = false;
+            }
+        }
+        else if ((A.hijoIzqdoB(n) != Abin<T>::NODO_NULO && A.hijoDrchoB(n) == Abin<T>::NODO_NULO) || (A.hijoIzqdoB(n) == Abin<T>::NODO_NULO && A.hijoDrchoB(n) != Abin<T>::NODO_NULO))
+        {
+            completo = false;
+        }
+        return completo && RamaDrch(A.hijoDrchoB(n), A) && RamaDrch(A.hijoIzqdoB(n), A);
+    }
+}
+
+template <typename T>
+bool esCompleto(typename Abin<T>::nodo n, const Abin<T> &A)
+{
+    return RamaIzqd(A.hijoIzqdoB(n), A) && RamaDrch(A.hijoDrchoB(n), A);
 }
 
 template <typename T>
 bool esAPO(const Abin<T> &A)
 {
-    cout << "\nMax desequilibrio: " << maxDesequilibrio(A.raizB(), A) << endl;
+    cout << "\nÁrbol completo: ";
+    esCompleto(A.raizB(), A) ? cout << "SÍ" << endl : cout << "NO" << endl;
     cout << "Son descendientes: ";
     sonDescendientes(A.raizB(), A) ? cout << "SÍ" << endl : cout << "NO" << endl;
 
-    return maxDesequilibrio(A.raizB(), A) == 0 && sonDescendientes(A.raizB(), A);
+    return esCompleto(A.raizB(), A) && sonDescendientes(A.raizB(), A);
 }
 
 /****************************************************/
@@ -58,8 +107,8 @@ int main()
 {
     Abin<tElto> A;
     cout << "\n*** Lectura de árbol binario A de abin.dat ***\n";
-    ifstream fe("Data/abin-completo.dat"); // abrir fichero de entrada
-    rellenarAbin(fe, A);          // desde fichero
+    ifstream fe("Data/abin_3.dat"); // abrir fichero de entrada
+    rellenarAbin(fe, A);                   // desde fichero
     fe.close();
     cout << "\n*** Mostrar árbol binario A 'abin-completo.data' ***\n";
     imprimirAbin(A); // en std::cout
