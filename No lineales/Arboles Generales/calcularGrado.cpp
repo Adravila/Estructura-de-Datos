2e1b7a176e71bd171768d8358a3d0fc662d39e33
Adrián <Adravila@users.noreply.github.com>
2019-04-26 01:33:29 +0200

    Update calcularGrado.cpp

--- No lineales/Arboles Generales/calcularGrado.cpp
+++ No lineales/Arboles Generales/calcularGrado.cpp
@@ -13,43 +13,30 @@ using namespace std;
 **/
 
 template <typename T>
-int numHijos(typename Agen<T>::nodo n, const Agen<T> &A)
+int gradoAgenRec(typename Agen<T>::nodo n, const Agen<T> &A)
 {
-    int nHijos;
     typename Agen<T>::nodo hijo;
+    int gradoMax;
+
     if (n == Agen<T>::NODO_NULO)
     {
         return 0;
     }
     else
     {
-        nHijos = 0;
+        gradoMax = 0;
         hijo = A.hijoIzqdo(n);
         while (hijo != Agen<T>::NODO_NULO)
         {
-            ++nHijos;
+            if (n != A.raiz()) // Para evitar que el nodo raíz pase más de una vez
+            {
+                ++gradoMax;
+            }
+            gradoMax = std::max(gradoMax, gradoAgenRec(hijo, A));
             hijo = A.hermDrcho(hijo);
         }
-        return nHijos;
+        return gradoMax;
     }
-}
-
-template <typename T>
-int gradoAgenRec(typename Agen<T>::nodo n, const Agen<T> &A)
-{
-    typename Agen<T>::nodo hijo;
-    int gradoMax = numHijos(n, A);
-    hijo = A.hijoIzqdo(n);
-
-    if (hijo == Agen<T>::NODO_NULO)
-        return 0;
-    else
-        while (hijo != Agen<T>::NODO_NULO)
-        {
-            gradoMax = std::max(gradoMax, gradoAgenRec(hijo,A));
-            hijo = A.hermDrcho(hijo);
-        }
-    return gradoMax;
 }
 
 template <typename T>
@@ -58,7 +45,7 @@ int gradoAgen(const Agen<T> &A)
     if (A.arbolVacio())
         return 0;
     else
-        return gradoAgenRec(A.raiz(),A);
+        return gradoAgenRec(A.raiz(), A);
 }
 
 int main()
@@ -66,7 +53,7 @@ int main()
     Agen<char> A, B, C, D;
     cout << "\n*** Lectura de árbol general A de agen.dat ***\n";
     ifstream fa("Data/agen.dat"); // abrir fichero de entrada
-    rellenarAgen(fa, A);     // desde fichero
+    rellenarAgen(fa, A);          // desde fichero
     fa.close();
     cout << "\n*** Mostrar árbol general A ***\n";
     imprimirAgen(A); // en std::cout
@@ -74,7 +61,7 @@ int main()
 
     cout << "\n*** Lectura de árbol general B de agen-ter.dat ***\n";
     ifstream fb("Data/agen-ter.dat"); // abrir fichero de entrada
-    rellenarAgen(fb, B);         // desde fichero
+    rellenarAgen(fb, B);              // desde fichero
     fb.close();
     cout << "\n*** Mostrar árbol general B ***\n";
     imprimirAgen(B); // en std::cout
@@ -82,7 +69,7 @@ int main()
 
     cout << "\n*** Lectura de árbol general C de agen-cua.dat ***\n";
     ifstream fc("Data/agen-cua.dat"); // abrir fichero de entrada
-    rellenarAgen(fc, C);         // desde fichero
+    rellenarAgen(fc, C);              // desde fichero
     fc.close();
     cout << "\n*** Mostrar árbol general C ***\n";
     imprimirAgen(C); // en std::cout
@@ -90,7 +77,7 @@ int main()
 
     cout << "\n*** Lectura de árbol general D de agen-abu.dat ***\n";
     ifstream fd("Data/agen-abu.dat"); // abrir fichero de entrada
-    rellenarAgen(fd, D);         // desde fichero
+    rellenarAgen(fd, D);              // desde fichero
     fd.close();
     cout << "\n*** Mostrar árbol general D ***\n";
     imprimirAgen(D); // en std::cout
