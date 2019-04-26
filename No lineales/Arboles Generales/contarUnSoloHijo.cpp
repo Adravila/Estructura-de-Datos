a05096d18b6d2259e6e8f323022cefe8f9624857
Adrián <Adravila@users.noreply.github.com>
2019-04-27 00:05:34 +0200

    Fixed - contarUnSoloHijo.cpp

--- No lineales/Arboles Generales/contarUnSoloHijo.cpp
+++ No lineales/Arboles Generales/contarUnSoloHijo.cpp
@@ -8,7 +8,7 @@ using namespace std;
 /**
  * Contar los nodos que tengan un solo hijo de todo el árbol general
  * Creado por: Adrián Dávila Guerra ©
- * 25/04/2019
+ * 26/04/2019
 **/
 
 template <typename T>
