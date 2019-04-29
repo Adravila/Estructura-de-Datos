4770c3f497151392ff56016431dc3d03d5fac8c4
Adrián <Adravila@users.noreply.github.com>
2019-04-29 22:25:07 +0200

    Update

---
+++ No lineales/Arboles Binario Busqueda/diccionario.cpp
@@ -0,0 +1,117 @@
+#include "Cabeceras/abb.hpp"
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Diccionario: Haz un diccionario usando un Arbol de búsqueda. Utiliza como estructura una
+// con los campos español y zuelandes por ejemplo y acuerdate de sobrecargar
+// los operandos de comparación de la estructura.
+
+struct Palabra
+{
+    Palabra() {}
+    Palabra(string esp, string zue) : espaniol(esp), zuelandes(zue) {}
+    string espaniol;
+    string zuelandes;
+};
+
+inline bool operator<(const Palabra &p1, const Palabra &p2)
+{
+    return p1.espaniol < p2.espaniol;
+}
+
+ostream &operator<<(ostream &os, const Palabra &p)
+{
+    return os << p.espaniol;
+}
+
+class Diccionario
+{
+    public:
+        Diccionario() {}
+        void insertarPalabra(const Palabra &pal);
+        void EliminarPalabra(const Palabra &pal);
+        void cambiarPalabra(Palabra &pal, string esp_ep);
+        string buscarPalabra(const Palabra &pal);
+        void ImprimirPalabras(const Palabra &e);    // Exclusivo
+    private:
+        Abb<Palabra> A;
+};
+
+void Diccionario::insertarPalabra(const Palabra &pal)
+{
+    A.insertar(pal);
+}
+
+void Diccionario::EliminarPalabra(const Palabra &pal)
+{
+    if (!A.buscar(pal).vacio())
+    {
+        A.eliminar(A.buscar(pal).elemento());
+    }
+}
+
+void Diccionario::cambiarPalabra(Palabra &pal, string pal_esp)
+{
+    if (!A.buscar(pal).vacio())
+    {
+        A.eliminar(pal);
+        pal.espaniol = pal_esp;
+        A.insertar(pal);
+    }
+}
+
+string Diccionario::buscarPalabra(const Palabra &pal)
+{
+    if (!A.buscar(pal).vacio())
+    {
+        return A.buscar(pal).elemento().zuelandes;
+    }
+    else
+        return "Palabra no encontrada";
+}
+
+void Diccionario::ImprimirPalabras(const Palabra &e)
+{
+    Abb<Palabra> B = A.buscar(e);
+    if (!B.vacio())
+    {
+        cout << "Español: " << B.elemento().espaniol << endl;
+        cout << "Zuelandes: " << B.elemento().zuelandes << endl;
+    }
+    else
+    {
+        cout << "No se ha encontrado la palabra" << endl;
+    }
+}
+
+int main()
+{
+    Palabra p1("A", "1");
+    Palabra p2("B", "2");
+    Palabra p3("C", "3");
+    Palabra p4("D", "4");
+    Palabra p5("E", "5");
+    Palabra p6("F", "6");
+    Palabra p7("G", "7");
+    Palabra p8("H", "8");
+    Palabra p9("I", "9");
+    Palabra p10("J", "10");
+    Diccionario D;
+    D.insertarPalabra(p1);
+    D.insertarPalabra(p2);
+    D.insertarPalabra(p3);
+    D.insertarPalabra(p4);
+    D.insertarPalabra(p5);
+    D.insertarPalabra(p6);
+    D.insertarPalabra(p7);
+    D.insertarPalabra(p8);
+    D.insertarPalabra(p9);
+    D.insertarPalabra(p10);
+    D.cambiarPalabra(p10, "Nueva Palabra");
+    cout << "\n\nDatos de p10\n"; // J se traduce a 10
+    D.ImprimirPalabras(p10);
+    cout << endl;
+}
---
+++ No lineales/Arboles Generales/rebeldes.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <fstream>
+#include "Cabeceras/Agen.hpp"
+#include "Cabeceras/agen_E-S.h"
+#include <math.h>
+
+using namespace std;
+
+/* Se dice de nodo rebelde aquel cuyas ideas son diferentes a la de la mayoría de sus ancestros.
+Dado un árbol binario cualquiera calcular el número de nodos rebeldes de dicho árbol.
+Ejemplo: si el nodo vale '1' y tiene tres ancestros que valen '1' y seis que valen '0', es rebelde. */
+
+template <typename T>
+int contarAncestros(typename Agen<T>::nodo n, const Agen<T> &A)
+{
+    if (n != A.raiz() || n != Agen<T>::NODO_NULO)
+    {
+        return 1 + contarAncestros(A.padre(n),A);
+    }
+}
+
+template <typename T>
+int num_true(typename Agen<T>::nodo n, const Agen<T> &A)
+{
+
+    if (n == Agen<T>::NODO_NULO)
+        return 0;
+    else if (A.padre(n))
+        return 1 + num_true(A.padre(n), A);
+    else
+        return num_true(A.padre(n), A);
+}
+
+template <typename T>
+bool esRebelde(typename Agen<T>::nodo n, const Agen<T> &A)
+{
+
+    if (A.elemento(n) == true)
+    {
+        return (contarAncestros(n, A) / 2) > (num_true(n, A));
+    }
+    else
+    {
+        return false;
+    }
+}
+
+template <typename T>
+int contarRebeldesRec(const Agen<T> &A, typename Agen<T>::nodo n)
+{
+    int cont = 0;
+    if (n == Agen<T>::NODO_NULO)
+    {
+        return 0;
+    }
+    else
+    {
+        typename Agen<T>::nodo hijo = A.hijoIzqdo(n);
+        while (hijo != Agen<T>::NODO_NULO)
+        {
+            if (esRebelde(hijo,A))
+            {
+                cont++;
+            }
+            cont += contarRebeldesRec(A, hijo);
+            hijo = A.hermDrcho(hijo);
+        }
+    }
+    return cont;
+}
+
+template <typename T>
+int contarRebeldes(const Agen<T> &A)
+{
+    return contarRebeldesRec(A, A.raiz());
+}
+
+int main()
+{
+    Agen<char> A, B, C, D;
+    cout << "\n*** Lectura de árbol general A de agen.dat ***\n";
+    ifstream fa("Data/agen.dat"); // abrir fichero de entrada
+    rellenarAgen(fa, A);          // desde fichero
+    fa.close();
+    cout << "\n*** Mostrar árbol general A ***\n";
+    imprimirAgen(A); // en std::cout
+    cout << "\nEncontrar nodo con elemento 'h' en el árbol A 'agen': " << contarRebeldes(A) << endl;
+
+    cout << "\n*** Lectura de árbol general B de agen-ter.dat ***\n";
+    ifstream fb("Data/agen-ter.dat"); // abrir fichero de entrada
+    rellenarAgen(fb, B);              // desde fichero
+    fb.close();
+    cout << "\n*** Mostrar árbol general B ***\n";
+    imprimirAgen(B); // en std::cout
+    cout << "\nEncontrar nodo con elemento 'h' en el árbol B 'agen-ter': " << contarRebeldes(B) << endl;
+
+    cout << "\n*** Lectura de árbol general C de agen-cua.dat ***\n";
+    ifstream fc("Data/agen-cua.dat"); // abrir fichero de entrada
+    rellenarAgen(fc, C);              // desde fichero
+    fc.close();
+    cout << "\n*** Mostrar árbol general C ***\n";
+    imprimirAgen(C); // en std::cout
+    cout << "\nEncontrar nodo con elemento 'b' en el árbol C 'agen-cua': " << contarRebeldes(C) << endl;
+
+    cout << "\n*** Lectura de árbol general D de agen-abu.dat ***\n";
+    ifstream fd("Data/agen-abu.dat"); // abrir fichero de entrada
+    rellenarAgen(fd, D);              // desde fichero
+    fd.close();
+    cout << "\n*** Mostrar árbol general D ***\n";
+    imprimirAgen(D); // en std::cout
+    cout << "\nEncontrar nodo con elemento 'x' en el árbol D 'agen-abu': " << contarRebeldes(D) << endl;
+}
