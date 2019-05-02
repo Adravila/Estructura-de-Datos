1d3ded92753c9b24e405c226dadefd578f5f4977
Adrián <Adravila@users.noreply.github.com>
2019-05-02 17:39:42 +0200

    Update

---
+++ No lineales/Grafos/Cabeceras/Propio/DijkstraInv.hpp
@@ -0,0 +1,60 @@
+#include <algorithm> //sort
+#include <vector>
+#include <stdlib.h>
+#include <cassert>
+#include <iostream>
+#include "../Grafo/grafoPMC.h"
+#include "../Grafo/alg_grafoPMC.h"
+#include "../Grafo/alg_grafo_E-S.h"
+
+using namespace std;
+
+template <typename tCoste>
+vector<tCoste> DijkstraInv(const GrafoP<tCoste>& G, typename GrafoP<tCoste>::vertice destino,
+                           vector<typename GrafoP<tCoste>::vertice>& P)
+{
+   typedef typename GrafoP<tCoste>::vertice vertice;
+   vertice v, w;
+   const size_t n = G.numVert();
+   vector<bool> S(n, false);                  // Conjunto de vértices vacío.
+   vector<tCoste> D;                          // Costes mínimos hasta destino.
+   
+   // Iniciar D y P con caminos directos hasta el vértice destino.
+   D = vector<tCoste> (n, GrafoP<tCoste>::INFINITO);
+   for(int i = 0; i < n; ++i)
+   {
+         D[i] = G[i][destino];
+   }
+   D[destino] = 0;                             // Coste destino-destino es 0.
+   P = vector<vertice>(n, destino);
+   
+   // Calcular caminos de coste mínimo desde cada vértice.
+   S[destino] = true;                          // Incluir vértice destino en S.
+   
+   for (size_t i = 1; i <= n; i++) {
+      // Seleccionar vértice w no incluido en S
+      // con menor coste hasta destino.
+      
+      tCoste costeMin = GrafoP<tCoste>::INFINITO;
+      for (v = 0; v < n; v++) {
+          if (!S[v] && D[v] <= costeMin) {
+             costeMin = D[v];
+             w = v;
+          }
+      }
+      S[w] = true;                          // Incluir vértice w en S.
+      
+      // Recalcular coste hasta cada v no incluido en S a través de w.
+      for (v = 0; v < n; v++) {
+          tCoste wvD;
+          if(D[v] != GrafoP<tCoste>::INFINITO) wvD = suma(G[v][w], D[w]);
+          else wvD = G[v][destino];
+          
+          if (wvD < D[v]) {
+             D[v] = wvD;
+             P[v] = (D[w] != GrafoP<tCoste>::INFINITO) ? w : v;
+          }
+      }
+   }
+   return D;
+}
--- No lineales/Grafos/Zuelandia.cpp
+++ No lineales/Grafos/Zuelandia.cpp
@@ -6,9 +6,59 @@
 #include "Cabeceras/Grafo/grafoPMC.h"
 #include "Cabeceras/Grafo/alg_grafoPMC.h"
 #include "Cabeceras/Grafo/alg_grafo_E-S.h"
+#include "Cabeceras/Propio/DijkstraInv.hpp"
 
 using namespace std;
 
+template <typename tCoste>
+GrafoP<tCoste> zuelandia(GrafoP<tCoste> &G, vector<bool> ciudadesTomadas,
+                         matriz<bool> carreterasTomadas, typename GrafoP<tCoste>::vertice capital)
+{
+    typedef typename GrafoP<tCoste>::vertice vertice;
+
+    // Reflejar en G ciudades tomadas por los rebeldes
+    for (vertice i = 0; i < G.numVert(); ++i)
+    {
+        if (ciudadesTomadas[i])
+        {
+            for (vertice v = 0; v < G.numVert(); ++v)
+            {
+                G[i][v] = GrafoP<tCoste>::INFINITO;
+                G[v][i] = GrafoP<tCoste>::INFINITO;
+            }
+        }
+    }
+
+    // Reflejar en G las carreteras que han sido cortadas por los rebeldes
+    for (vertice v = 0; v < G.numVert(); ++v)
+    {
+        for (vertice w = 0; w < G.numVert(); ++w)
+        {
+            if (carreterasTomadas[v][w])
+                G[v][w] = GrafoP<tCoste>::INFINITO;
+        }
+    }
+
+    // Bonus: Si no tuvieramos en cuenta la ciudad capital se podría
+    // matriz<vertice> Camino(G.numVert());
+    // return Floyd(G,Camino);
+    
+    vector<GrafoP<int>::vertice> PDisj, PInv;
+    vector<tCoste> CMHastacapital = DijkstraInv(G, capital, PInv);
+    vector<tCoste> CMDesdecapital = Dijkstra(G, capital, PDisj);
+
+    GrafoP<tCoste> sol(G.numVert());
+    
+    for (size_t i = 0; i < G.numVert(); i++) {
+        for (size_t j = 0; j < G.numVert(); j++) {
+            if(i == j)
+                sol[i][j] = 0;
+            else
+                sol[i][j] = suma(CMHastacapital[i], CMDesdecapital[j]);
+        }
+    }
+    return sol;
+}
 
 int main()
 {
@@ -17,10 +67,25 @@ int main()
      * Floyd para obtener la matriz de costes mínimos de cada vértice desde un origen a un destino
      * En el apartado D se realizará Dijsktra y Dijsktra inverso (Para todo origen -> Capital + Capital -> Para todo destino)
     **/
-   
-    GrafoP<int> G("Data/grafo-3.txt");
-    std::cout << G;
 
+    GrafoP<int> G("Data/zuelandia.txt");
+    std::cout << G;
     vector<GrafoP<int>::vertice> v;
-    std::cout << "\nResultado Diámetro: " << Diametro(G) << endl;
+
+    // Crear una matriz de booleanos que refleje las ciudades tomadas
+    vector<bool> ciudadesTomadas(G.numVert(), false);
+    // Los rebeldes toman las dos últimas ciudades
+    ciudadesTomadas[G.numVert() - 1] = true;
+    ciudadesTomadas[G.numVert() - 2] = true;
+
+    // Carreteras tomadas (inicialimente como falso)
+    matriz<bool> carrreterasCortadas(G.numVert(), false);
+    // Los rebeldes explotaron el puente de A --> B
+    carrreterasCortadas[3][1] = true;
+
+    // Capital
+    typename GrafoP<int>::vertice capital = 0;
+
+    // Resultado
+    std::cout << "\nResultado: " << zuelandia(G, ciudadesTomadas, carrreterasCortadas, capital) << endl;
 }
