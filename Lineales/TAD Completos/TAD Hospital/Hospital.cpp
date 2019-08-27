ad36a0cd9c8c52a311350ff3a12b3e6c4e174914
Adrián <Adravila@users.noreply.github.com>
2019-08-27 14:53:20 +0200

    Update

--- Lineales/TAD Completos/TAD Hospital/Hospital.cpp
+++ Lineales/TAD Completos/TAD Hospital/Hospital.cpp
@@ -20,6 +20,7 @@ class Hospital
 {
 public:
     explicit Hospital(unsigned int tam_uci, unsigned int tam_planta);
+    void ingresoPacienteMAL(unsigned int id, unsigned int grav);
     void ingresoPaciente(unsigned int id, unsigned int grav);
     void altaPaciente();
     void matarPaciente(unsigned int id);
@@ -46,24 +47,30 @@ Hospital::Hospital(unsigned int tam_uci, unsigned int tam_planta)
       Lu{Lista<tPaciente>{tam_uci}},
       current_p(0), current_u(0) {}
 
-// Precondición: La gravedad debe ser entre 0 y 9, id del paciente debe ser distinto.
+// Precondición: La gravedad debe ser entre 0 y 9, id del paciente debe ser distinto
 // Postcondición: Ingresa un paciente en la planta según su gravedad y espacio
 void Hospital::ingresoPaciente(unsigned int id, unsigned int grav)
 {
     assert(grav < 10); // La gravedad no puede ser mayor que 9 (0-9)
-    assert(current_u < tam_u || current_p < tam_p);
 
     Lista<tPaciente>::posicion p_u, p_p;
     tPaciente pa(id, grav); // Creamos el paciente
+    tPaciente pw = pa;
 
     p_u = Lu.buscar(pa);
     p_p = Lp.buscar(pa);
-    assert(p_u == Lu.fin() && p_p == Lp.fin()); // Si coincide ID se termina (¿?¿¿)
+    assert(p_u == Lu.fin() && p_p == Lp.fin()); // Si coincide ID se termina
 
     if (grav == 0) // Paciente entra en la morgue
     {
         cout << "El paciente está en la morgue." << endl;
         return; // Para el procedimiento.
+    }
+
+    if (current_p == tam_p && current_u == tam_u) // Hospital lleno
+    {                                             // Damos de alta al paciente con mejor estado de salud
+        Lp.eliminar(Lp.anterior(Lp.fin()));
+        --current_p;
     }
 
     if (grav > 0 && grav < 6) // Ingresar en la UCI
@@ -78,40 +85,47 @@ void Hospital::ingresoPaciente(unsigned int id, unsigned int grav)
             return;
         }
 
-        if (current_u >= tam_u) // Si está llena la UCI, se traslada el paciente menos grave
-        {                       // a la planta (primera prioridad según su gravedad)
-            tPaciente pw = Lu.elemento(Lu.anterior(Lu.fin()));
-            Lu.eliminar(Lu.anterior(Lu.fin()));
-            Lp.insertar(pw, Lu.primera());
-            --current_u;
-            ++current_p;
-        }
         while (p_u != Lu.fin())
         {
             if (grav < Lu.elemento(p_u).grav)
             {
-                Lu.insertar(pa, p_u);
-                ++current_u;
-                return;
+                if(current_u < tam_u)
+                {
+                    Lu.insertar(pa, p_u);
+                    ++current_u;
+                    return;
+                }
+                else
+                {
+                    pw = Lu.elemento(Lu.anterior(Lu.fin()));
+                    Lu.eliminar(Lu.anterior(Lu.fin()));
+                    Lu.insertar(pa, p_u);
+                    p_u = Lu.fin();
+                }
             }
             else
             {
                 p_u = Lu.siguiente(p_u);
                 if (p_u == Lu.fin())
                 {
-                    Lu.insertar(pa, p_u);
-                    ++current_u;
-                    return;
+                    if(current_u < tam_u)
+                    {
+                        Lu.insertar(pa, p_u);
+                        ++current_u;
+                        return;
+                    }
                 }
             }
         }
     }
 
-    // Si continua, es porque no se ha añadido ningún paciente,
+    // Si continua, es porque no se ha añadido ningún paciente o ha sido trasladado
     // Vamos a añadirlo en la Planta
-    p_p = Lp.primera();
 
-    if (p_p == Lp.fin()) // No hay pacientes en la UCI
+    p_p = Lp.primera();
+    pa = pw;    // Trasladar paciente a la Planta
+
+    if (p_p == Lp.fin()) // No hay pacientes en la Planta
     {
         Lp.insertar(pa, p_p);
         ++current_p;
@@ -138,6 +152,7 @@ void Hospital::ingresoPaciente(unsigned int id, unsigned int grav)
             }
         }
     }
+
 }
 
 // Postcondición: Dar de alta un paciente con el mejor estado de salud
@@ -160,10 +175,8 @@ void Hospital::altaPaciente()
         if (p_u != Lu.primera()) // La UCI no está vacía, pero la planta sí
         {
             p_u = Lu.anterior(p_u);
-            Lp.insertar(Lu.elemento(p_u), Lp.primera());
             Lu.eliminar(p_u);
             --current_u;
-            ++current_p;
         }
         else
         {
@@ -288,10 +301,32 @@ int main()
     h.ingresoPaciente(1, 1);
     h.ingresoPaciente(2, 4);
     h.ingresoPaciente(3, 3);
-    h.ingresoPaciente(4, 5);
+    h.ingresoPaciente(4, 2);
     h.ingresoPaciente(5, 2);
+    h.ingresoPaciente(6, 8);
+    h.ingresoPaciente(7, 4);
+    h.altaPaciente();
+    h.altaPaciente();
+    h.altaPaciente();
+    h.altaPaciente();
+    h.altaPaciente();
+    h.altaPaciente();
+    h.altaPaciente();
+    h.altaPaciente();
+    h.altaPaciente();
+    h.ingresoPaciente(1, 4);
+    h.ingresoPaciente(2, 3);
+    h.ingresoPaciente(3, 1);
+    h.ingresoPaciente(4, 6);
+    h.altaPaciente();
+    h.ingresoPaciente(6, 2);
+    h.ingresoPaciente(7, 3);
+    h.ingresoPaciente(8, 8);
+    h.altaPaciente();
+    h.ingresoPaciente(9, 9);
+
     h.pacientesUCI();
     h.pacientesPlanta();
-    h.pacientesGravedad(1);
+    h.pacientesGravedad(4);
     return 0;
 }
